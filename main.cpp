8e1c6489de111c2e50817bdb53d1d0e64a9cac62
pinguX <pinguX@yandex.com.tr>
2023-10-16 19:35:00 -0400

    stylesheet

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //a.setStyleSheet(css_stil);
+    a.setStyleSheet(css_stil_android);
     MainWindow win;
     win.show();
     return a.exec();
